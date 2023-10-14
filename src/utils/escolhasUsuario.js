050d17f56e981069b0590eb8c34d109857589fe1
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2023-10-14 12:49:13 -0300

    adicionando espaço para funções

---
+++ src/utils/escolhasUsuario.js
@@ -0,0 +1,20 @@
+const escolherDownload = (tipoArquivo) => {
+    
+    return tipoEscolhido
+};
+
+const escolherNome = (nomeArquivo) => {
+
+    return nomeEscolhido
+};
+
+const escolherQualidade = (qualidadeDownload) => {
+    
+    return qualidadeEscolhida
+};
+
+module.exports = {
+    escolherDownload,
+    escolherNome,
+    escolherQualidade,
+};
