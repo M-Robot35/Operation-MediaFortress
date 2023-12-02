325a3663ddd727ec1227fcbadb28256f9bfa66eb
M-Robot35 <thiago.teles725@gmail.com>
2023-12-01 22:21:49 -0300

    fork

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
-const path = require('path')
+const cluster = require("cluster");
+const os = require("os");
 
-const cluster = require('cluster')
-const os = require('os')
+function clusterMaster() {
+  const cpus = os.cpus().length * 2;
 
-function clusterMaster(){
-    const cpus = os.cpus().length * 2
-    
-    //console.log(`Primary ${process.pid} is running`)
-    //console.log(`Fork ${cpus}`)
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
 
-    for(let i=0; i < cpus; i++){
-        cluster.fork()
+  cluster.on("exit", (worker, code, signal) => {
+    if (code !== 0 && !worker.exitedAfterDisconnect) {
+      cluster.fork();
     }
-
-    cluster.on('exit',(worker, code, signal)=>{
-        if(code !== 0  &&  !worker.exitedAfterDisconnect){
-            cluster.fork()
-        }
-    })
+  });
 }
 
-async function clusterSlave(){
-    await require('./src/server')
+async function clusterSlave() {
+  await require("./src/server");
 }
 
-cluster.isMaster ? clusterMaster(): clusterSlave()
-
+cluster.isMaster ? clusterMaster() : clusterSlave();
